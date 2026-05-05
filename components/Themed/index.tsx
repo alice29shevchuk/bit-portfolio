0add314fdfe11715daec7c9e0b6543b6aaf3d6d8
alice29shevchuk <Sevc_sd76@student.itstep.org>
2026-05-05 11:02:40 +0300

    Update structure

---
+++ components/Themed/index.tsx
@@ -0,0 +1,3 @@
+export { useClientOnlyValue } from './useClientOnlyValue';
+export { useColorScheme } from './useColorScheme';
+export { Text, View, useThemeColor, type TextProps, type ViewProps } from './Themed';
